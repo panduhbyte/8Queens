6652655e484761c90c386e2666baf9c96c2f3fad
Char-kun <panduhbyte@gmail.com>
2017-09-20 20:38:47 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/8Queens.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8Queens.dir/main.cpp.obj"
+  "8Queens.pdb"
+  "8Queens.exe"
+  "8Queens.exe.manifest"
+  "lib8Queens.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8Queens.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
